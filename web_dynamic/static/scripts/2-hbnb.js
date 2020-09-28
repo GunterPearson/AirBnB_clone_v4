035ceb97b501529ef8d5a7e6a6e6c422af95a3c2
GunterPearson <1715@holbertonschool.com>
2020-09-28 14:21:44 -0500

    added corrections to 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,13 @@
 $('document').ready(function () {
+  const amenities = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities H4').text(Object.values(amenities).join(', '));
+  });
   $.get('http://127.0.0.1:5001/api/v1/status', function (data) {
     if (data.status === 'OK') {
       $('div#api_status').addClass('available');
